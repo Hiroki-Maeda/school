6efc86a16ecaef18160f463a807f7b82e7f12e62
前田　浩貴 <maedahiroki@Bio-ZnoMacBook-Air.local>
2017-10-19 17:39:53 +0900

    1013_update

--- network/1013/ex6.c
+++ network/1013/ex6.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
 int main(int argc,char *argv[]){
-  char line;
-
-  FILE *fp;
-  if((fp = fopen(argv[1], "r")) == NULL){
-    printf("file open error\n");
-    return 1;
-  }
-
-  while(fgetc(line = fgetc(fp)) != EOF){
-    printf("%s",line);
-    if(ferror(fp)==1){
-      printf("IO Exception!\n");
+    char line[256];
+    int i;
+    FILE *fp;
+    if((fp = fopen(argv[1], "r")) == NULL){
+        printf("file open error\n");
+        return 1;
     }
-  }
-  fclose(fp);
-  return 0;
+
+    while(1){
+           
+        line[i++] = fgetc(fp);
+        if(feof(fp)){
+            printf("break%d\n",i);
+            break;
+        }else{
+            printf("feof=%d\n",feof(fp));
+        }
+        if(ferror(fp)){
+            printf("IO Exception!\n");
+        }    
+   
+    }
+    printf("%s",line);
+    fclose(fp);
+    
+    return 0;
 }
     
     
